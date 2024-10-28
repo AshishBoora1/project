f76a3a8210e69f47986adb8975816d412a8fbc94
ashish <boora8837@gamil.com>
2024-10-28 16:30:44 +0530

    scale pop

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,15 @@ function App() {
       {showsignpop && (
         <div className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-md z-[80]"></div>
       )}
-      {showsignpop && (
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100] w-full md:w-auto">
-          <SignUp />
-        </div>
-      )}
+      {/* {showsignpop && ( */}
+      <div
+        className={`fixed top-1/2 left-1/2 -translate-x-1/2 duration-700 -translate-y-1/2 z-[100] w-full md:w-auto ${
+          !showsignpop ? " scale-0" : " scale-100"
+        }`}
+      >
+        <SignUp />
+      </div>
+      {/* )} */}
       <Routes>
         <Route index element={<Home />} />
         <Route path="my-profile" element={<MyProfile />} />
